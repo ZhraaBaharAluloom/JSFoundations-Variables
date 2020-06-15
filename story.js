c1dfcf56f5cd6fa880112cbb5df2ac7a1bf97d54
ZhraaBaharAluloom <alwarda@hotmail.co.uk>
2020-06-15 17:48:26 +0300

    story.js

--- story.js
+++ story.js
@@ -5,3 +5,7 @@
 // Define the hobby here on line 6
 
 // Print the story here on line 8
+let name = "Zahraa";
+let age = 24;
+let hobby = "writing";
+console.log (`Miss. ${name}, in 33 years, you'll be ${age+33} years old. By then you'll decades of ${hobby}  `);
